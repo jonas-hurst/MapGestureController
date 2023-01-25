4b764dd0ae2c32b2c0effa748c81e33a288cc7d2
hurst <jonhur@web.de>
2023-01-25 12:20:19 +0100

    added class to extend guibase

---
+++ src/guibaseExtended.py
@@ -0,0 +1,13 @@
+from guibase import Guibase
+
+
+class GuibaseExtended(Guibase):
+    """
+    Class to extend Guibase.
+    Guibase is automatically generated from wxFormBuilder.
+    This class extends the GUI with GUI-Elements that are not supported by wxFormBuilder.
+    gui.MainWindow inherits from GuibaseExtended to implement functionality.
+    """
+
+    def __init__(self, parent):
+        Guibase.__init__(self, parent)

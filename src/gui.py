7468c69d0cf406bcc66a2269c2909d039ed33e90
hurst <jonhur@web.de>
2023-01-27 13:57:52 +0100

    outsourced GUI actions into guibaseExtended class

--- src/gui.py
+++ src/gui.py
@@ -1,39 +1,28 @@
-import wx
-from guibase import Guibase
+from guibaseExtended import GuibaseExtended
 import threading
 from mpTest2 import TrackerController
-import numpy as np
-from cv2 import resize,cvtColor, COLOR_BGR2RGB
 
 
-class MainWindow(Guibase):
+class MainWindow(GuibaseExtended):
     def __init__(self, parent):
-        Guibase.__init__(self, parent)
-
-        width, height = 854, 480
-
-        frame = np.zeros((width, height, 3))
-        self.bmp = wx.Bitmap.FromBuffer(width, height, frame)
-        self.image_container.SetInactiveBitmap(self.bmp)
-        self.image_container.SetBackgroundStyle(wx.BG_STYLE_PAINT)
+        GuibaseExtended.__init__(self, parent)
 
         self.__tracker_controller = TrackerController(visualize=True)
 
-    def on_tgl_camera( self, event ):
+    def on_tgl_camera(self, event):
         btn_value = self.tgl_btn_start_camera.Value
         if btn_value:
             self.start_camera()
         else:
             self.stop_camera()
-        self.tgl_btn_show_feed.Enable(btn_value)
-        self.tgl_btn_touchcontrol.Enable(btn_value)
+        GuibaseExtended.on_tgl_camera(self, event)
 
-    def on_close( self, event ):
+    def on_close(self, event):
         if self.__tracker_controller.camera_running:
             self.stop_camera()
-        wx.Exit()
+        GuibaseExtended.on_close(self, event)
 
-    def start_camera( self ):
+    def start_camera(self):
         camera_thread = threading.Thread(target=self.cameraloop, daemon=True)
         self.__tracker_controller.initialize_tracking()
         camera_thread.start()
@@ -48,10 +37,3 @@ class MainWindow(Guibase):
             self.set_bitmap(self.__tracker_controller.color_image_bgr)
             if not self.__tracker_controller.camera_running:
                 break
-
-    def set_bitmap(self, frame):
-        frame_small = resize(frame.copy(), (854, 480))
-        frame_small_rgb = cvtColor(frame_small, COLOR_BGR2RGB)
-        self.bmp.CopyFromBuffer(frame_small_rgb)
-        self.image_container.SetInactiveBitmap(self.bmp)
-        #self.image_container.Refresh()
---
+++ src/guibaseExtended.py
@@ -0,0 +1,40 @@
+import wx
+from cv2 import resize,cvtColor, COLOR_BGR2RGB
+import numpy as np
+
+from guibase import Guibase
+
+
+class GuibaseExtended(Guibase):
+    def __init__(self, parent):
+        Guibase.__init__(self, parent)
+
+        width, height = 854, 480
+        frame = np.zeros((width, height, 3))
+        self.bmp = wx.Bitmap.FromBuffer(width, height, frame)
+        self.image_container.SetInactiveBitmap(self.bmp)
+        self.image_container.SetBackgroundStyle(wx.BG_STYLE_PAINT)
+
+    def on_tgl_camera(self, event):
+        btn_value = self.tgl_btn_start_camera.Value
+        self.tgl_btn_show_feed.Enable(btn_value)
+        self.tgl_btn_touchcontrol.Enable(btn_value)
+
+    def on_close(self, event):
+        wx.Exit()
+
+    def set_datagrid_values(self, tabledata: dict):
+        if self.infogrid.NumberRows != len(tabledata):
+            self.infogrid.AppendRows(len(tabledata), False)
+
+        rowcount = 0
+        for idx, key in enumerate(tabledata):
+            self.infogrid.SetCellValue(idx, 0, key)
+            self.infogrid.SetCellValue(idx, 1, str(tabledata[key]))
+            rowcount += 1
+
+    def set_bitmap(self, frame):
+        frame_small = resize(frame.copy(), (854, 480))
+        frame_small_rgb = cvtColor(frame_small, COLOR_BGR2RGB)
+        self.bmp.CopyFromBuffer(frame_small_rgb)
+        self.image_container.SetInactiveBitmap(self.bmp)

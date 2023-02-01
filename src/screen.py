63b79c81ed6ac13a660fbeaddd4e33d5a7858800
jonas-hurst <jonhur@web.de>
2023-02-01 12:00:04 +0100

    completed contains_point check

--- src/screen.py
+++ src/screen.py
@@ -2,23 +2,61 @@ from geom import *
 from numbers import Real
 
 
-class Screen(Plane3D):
-    def __init__(self, a: Real, b: Real, c: Real, d: Real, px_width: int, px_height: int, mm_width: int, mm_height: int):
-        # TODO: Hard-code Sceren constraints (e.g. is always an upright plain)
-        # TODO: Implement screen starting point (lower left corner?)
+class Screen:
+    def __init__(self, lower_left_corner: Point3D, upper_right_corner: Point3D, px_width: int, px_height: int):
 
-        Plane3D.__init__(self, a, b, c, d)
+        # TODO: Check if lower and upper corner are actually lower and upper left/right
+        # TODO Check if screen points  are directly upwards
+
+        self.lower_left_corner: Point3D = lower_left_corner
+        self.upper_right_corner: Point3D = upper_right_corner
+
+        self.screen_plain = Plane3D.from_vectors(self.lower_left_corner.get_pointvector(),
+                                                 Vector3D.from_points(lower_left_corner, upper_right_corner),
+                                                 Vector3D(0, -1, 0)   # Third vector just points upwards
+                                                 )
 
         self.px_width = px_width
         self.px_height = px_height
 
-        self.mm_width = mm_width
-        self.mm_height = mm_height
+    def contains_point(self, point: Point3D) -> bool:
+        """
+        Checks if a Point is on the screen or not.
+        :param point: The point to check
+        :return: bool value indicating if point is on screen
+        """
 
-    def contains_point(self, pnt: Point3D, epsilon: float = 0.0001) -> bool:
-        if not Plane3D.contains_point(self, pnt, epsilon):
+        # Check if point is contained within the screen plain
+        if not self.screen_plain.contains_point(point, epsilon=0.0001):
             return False
-        #TODO: Check if point is on screen
+
+        # Check if point is within the screen's bounding box
+        if not self.check_point_in_screen_bbox(point):
+            return False
+
+        return True
+
+    def check_point_in_screen_bbox(self, point: Point3D) -> bool:
+        """
+        Checks if point is located within the screens bbox.
+        Bbox is defined by self.lower_left_corner and self.upper_right_corner in 3D space
+        :param point: Point to be checked
+        :return: Bool value indicating if point is within bbox
+        """
+
+        if not self.lower_left_corner.x <= point.x <= self.upper_right_corner.x:
+            print("check1 False")
+            return False
+
+        if not self.lower_left_corner.y <= point.y <= self.upper_right_corner.y:
+            print("check2 False")
+            return False
+
+        if not self.lower_left_corner.z <= point.z <= self.upper_right_corner.z:
+            print("check3 False")
+            return False
+
+        return True
 
     def draw_point(self):
         #TODO: Draw intersection point on screen

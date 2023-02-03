7a7250c75424fb742bade35a61effc016132a342
jonas-hurst <jonhur@web.de>
2023-02-03 19:46:48 +0100

    added small example to show how server is used

--- src/websocketserver.py
+++ src/websocketserver.py
@@ -25,3 +25,53 @@ class Server(object):
 
 def _new_client(client, server):
     print("new client")
+
+# if __name__ == "__main__":
+#     # Simple standalone example of this module sending randomized dummy data
+#     from random import randint
+#     from time import sleep
+#
+#     s = Server()
+#     s.open_server()
+#
+#     msg = {
+#         "right": {
+#             "present": True,
+#             "position": {
+#                 "x": 0,
+#                 "y": 0
+#             }
+#         },
+#         "left": {
+#             "present": True,
+#             "position": {
+#                 "x": 0,
+#                 "y": 0
+#             }
+#         }
+#     }
+#
+#     try:
+#         while True:
+#
+#             # randomize right hand input
+#             if randint(0, 100) > 20:
+#                 msg["right"]["present"] = True
+#                 msg["right"]["position"]["x"] = randint(0, 1920)
+#                 msg["right"]["position"]["y"] = randint(0, 1000)
+#             else:
+#                 msg["right"]["present"] = False
+#
+#             # randomize left hand input
+#             if randint(0, 100) > 20:
+#                 msg["left"]["present"] = True
+#                 msg["left"]["position"]["x"] = randint(0, 1920)
+#                 msg["left"]["position"]["y"] = randint(0, 1000)
+#             else:
+#                 msg["left"]["present"] = False
+#
+#             s.send_json(msg)
+#             sleep(1)
+#
+#     except KeyboardInterrupt:
+#         s.close_server()
